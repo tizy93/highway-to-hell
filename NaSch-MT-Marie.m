23f508b77e8a303befd24f6b4e2bc7eb3d351be2
tizy93 <tiziana.ricciardi93@gmail.com>
2023-07-04 20:49:25 +0200

    titel geändert und kommentare hinzugefügt

--- NaSch-MT-Marie.m
+++ NaSch-MT-Marie.m
@@ -1,7 +1,7 @@
 (* ::Package:: *)
 
 (* ::Title:: *)
-(*Die A3*)
+(*The highway to hell*)
 
 
 (* ::Subtitle:: *)
@@ -24,30 +24,37 @@ Maximalgeschwindigkeit vMax und Tr\[ODoubleDot]delwahrscheinlichkeit p mit Funkt
 
 (*Autos haben Position x und Geschwindigkeit v zum vorderen Auto*)
 xAutos=Sort[RandomSample[Range[nCells],nCar]];
-vAutos=RandomInteger[{0,vMax},nCar]; 
+(**eine zuf\[ADoubleDot]llige Liste (Position) "xAutos" ohne wiederholungen (Sample) und in aufsteigende Reihen sortiert (Sort)erzeugen**)
+vAutos=RandomInteger[{0,vMax},nCar];
+(**eine zuf\[ADoubleDot]llige Zahl zu jedes Auto zuordnen **)
 
 (*Verkehrsregeln aus NaSch-Modell implementieren*)
 For[i=0,i<=tMax,i++, (*Schleife der Runden bis tMax*)
+(**Zyklus von Runden von i=0 bis i<=tMax**)
 
 (*Freie Zellen d vor dem Auto bis zum vorderen*)
 dAutos=Table[If[xAutos[[n]]<Max[xAutos],xAutos[[n+1]]-xAutos[[n]]-1,nCells-xAutos[[n]]+Min[xAutos]-1],{n,1,nCar-1}]; (*In Schleife, damit es geupdatet wird*)
+(**abstand des Autos mit bedingung dass ein auto kleiner gleich des gr\[ODoubleDot]\[SZ]es xAutos ist, dann ergibt den abstand zwischen zwei Autos **)
 (*Arrays fangen bei 1 an; n+1 muss f\[UDoubleDot]r das letzte gleich nCar sein; Abstand des letzten Autos ist bis zum ersten, da Ringstra\[SZ]e*)
 AppendTo[dAutos,If[xAutos[[nCar]]<Max[xAutos],xAutos[[1]]-xAutos[[nCar]]-1,nCells-xAutos[[nCar]]+Min[xAutos]-1]];
+(**abstand zur n\[ADoubleDot]chste auto (dar\[UDoubleDot]ber bin ich nicht sicher)????**)
 (*Abstand des Autos an Stelle nCar zum ersten wird angeh\[ADoubleDot]ngt;
 d immer -1, da freie Zellen vor Auto gemeint sind*)
 
 (*R1: Beschleunigen, falls vMax noch nicht erreicht*)
 vAutos=Table[Min[vAutos[[n]]+1,vMax],{n,1,nCar}];
-
+(**die geschwindigkeit wird implementiert von +1 jede Runde bis zu vMax**)
 (*R2: Abbremsen, falls v gr\[ODoubleDot]\[SZ]er als Abstand d*)
 vAutos=Table[Min[dAutos[[n]],vAutos[[n]]],{n,1,nCar}]; 
-
+(**wird \[CapitalUDoubleDot]berpr\[UDoubleDot]ft, ob der Abstand zwischen ein Auto und die vordere gro\[SZ] genug ist **)
 (*R3: Tr\[ODoubleDot]deln mit Wahrscheinlichkeit p*)
 vAutos=Table[If[RandomReal[{0,1}]<=p,vAutos[[n]]=Max[vAutos[[n]]-1,0],vAutos[[n]]],{n,1,nCar}]; (*Tr\[ODoubleDot]deln solange noch nicht v=0*)
+(**\[CapitalUDoubleDot]berpr\[UDoubleDot]f, ob die Tr\[ODoubleDot]delnwahrscheinligkeit (p) null oder eins ist also ob das auto tr\[ODoubleDot]delt oder nicht **)
 (*Falls zuf\[ADoubleDot]llige Zahl nicht im gegebenen Intervall, bleibt v gleich*)
 
 (*R4: Fahren um vAutos Zellen*)
 xAutos=Table[If[xAutos[[n]]+vAutos[[n]]<=nCells,xAutos[[n]]=xAutos[[n]]+vAutos[[n]],xAutos[[n]]=xAutos[[n]]+vAutos[[n]]-nCells],{n,1,nCar}];
+(**\[CapitalUDoubleDot]berpr\[UDoubleDot]f, ob die Tr\[ODoubleDot]delnwahrscheinligkeit (p) null oder eins ist also ob das auto tr\[ODoubleDot]delt oder nicht **)
 (*Falls Autos au\[SZ]erhalb Zellen bewegt, wird Bewegung in erster Zelle fortgesetzt, da Ringstra\[SZ]e*)
 
 {iter=Sow[xAutos,Print[xAutos]]} (*Gibt nach jedem Durchlauf der for-Schleife xAutos aus*)
@@ -58,10 +65,6 @@ xAutos=Table[If[xAutos[[n]]+vAutos[[n]]<=nCells,xAutos[[n]]=xAutos[[n]]+vAutos[[
 
 
 NaSch[10,30,5,5,0.3]
-
-
-(* ::Print:: *)
-(*{0,1,9,0,1,2,2,2,2,1}*)
 
 
 (* ::Text:: *)
@@ -148,10 +151,6 @@ vAutos=Table[Min[vAutos[[n]]+1,vMax],{n,1,nCar}]
 vAutos=Table[If[n<nCar,Min[dAutos[[n]],vAutos[[n]]],Min[nCells-xAutos[[n]]+xAutos[[1]],vAutos[[n]]]],{n,1,nCar}]
 vAutos=Table[If[RandomReal[{0,1}]<=p,vAutos[[n]]=Max[vAutos[[n]]-1,0],vAutos[[n]]],{n,1,nCar}]
 xAutos=Table[If[xAutos[[n]]+vAutos[[n]]<=nCells,xAutos[[n]]=xAutos[[n]]+vAutos[[n]],xAutos[[n]]=xAutos[[n]]+vAutos[[n]]-nCells],{n,1,nCar}]
-
-
-(* ::Input:: *)
-(**)
 
 
 (* ::Input:: *)
