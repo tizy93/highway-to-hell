000060af286432b15774e9e127137bd0ac017e27
tizy93 <tiziana.ricciardi93@gmail.com>
2023-07-05 16:28:35 +0200

    comment update

--- NaSch-MT-Marie.m
+++ NaSch-MT-Marie.m
@@ -24,13 +24,12 @@ Maximalgeschwindigkeit vMax und Tr\[ODoubleDot]delwahrscheinlichkeit p mit Funkt
 
 (*Autos haben Position x und Geschwindigkeit v zum vorderen Auto*)
 xAutos=Sort[RandomSample[Range[nCells],nCar]];
-(** erzeugt eine zuf\[ADoubleDot]llige Liste (Position) "xAutos" ohne wiederholungen (Sample) und in aufsteigende Reihen sortiert (Sort)**)
+(** erzeugt eine zuf\[ADoubleDot]llige (Random) Liste (Position) "xAutos" ohne wiederholungen (Sample) und in aufsteigende Reihen sortiert (Sort)**)
 vAutos=RandomInteger[{0,vMax},nCar];
 (**zuordnet eine zuf\[ADoubleDot]llige Zahl zu jedes Auto **)
 
 (*Verkehrsregeln aus NaSch-Modell implementieren*)
 For[i=0,i<=tMax,i++, (*Schleife der Runden bis tMax*)
-(**erzeugt ein Zyklus von Runden von i=0 bis i<=tMax**)
 
 (*Freie Zellen d vor dem Auto bis zum vorderen*)
 dAutos=Table[If[xAutos[[n]]<Max[xAutos],xAutos[[n+1]]-xAutos[[n]]-1,nCells-xAutos[[n]]+Min[xAutos]-1],{n,1,nCar-1}]; (*In Schleife, damit es geupdatet wird*)
@@ -43,18 +42,17 @@ d immer -1, da freie Zellen vor Auto gemeint sind*)
 
 (*R1: Beschleunigen, falls vMax noch nicht erreicht*)
 vAutos=Table[Min[vAutos[[n]]+1,vMax],{n,1,nCar}];
-(**die geschwindigkeit wird implementiert von +1 jede Runde bis zu vMax**)
 (*R2: Abbremsen, falls v gr\[ODoubleDot]\[SZ]er als Abstand d*)
 vAutos=Table[Min[dAutos[[n]],vAutos[[n]]],{n,1,nCar}]; 
 (**wird \[CapitalUDoubleDot]berpr\[UDoubleDot]ft, ob der Abstand zwischen ein Auto und die vordere gro\[SZ] genug ist **)
 (*R3: Tr\[ODoubleDot]deln mit Wahrscheinlichkeit p*)
 vAutos=Table[If[RandomReal[{0,1}]<=p,vAutos[[n]]=Max[vAutos[[n]]-1,0],vAutos[[n]]],{n,1,nCar}]; (*Tr\[ODoubleDot]deln solange noch nicht v=0*)
-(**\[CapitalUDoubleDot]berpr\[UDoubleDot]f, ob die Tr\[ODoubleDot]delnwahrscheinligkeit (p) null oder eins ist also ob das auto tr\[ODoubleDot]delt oder nicht **)
+(**RandomReal erzeugt ein zuf\[ADoubleDot]llige zahl zwischen 0 und 1, falls das unter p=0,3 liegt dan tr\[ODoubleDot]delt das auto sonst nicht **)
 (*Falls zuf\[ADoubleDot]llige Zahl nicht im gegebenen Intervall, bleibt v gleich*)
 
 (*R4: Fahren um vAutos Zellen*)
 xAutos=Table[If[xAutos[[n]]+vAutos[[n]]<=nCells,xAutos[[n]]=xAutos[[n]]+vAutos[[n]],xAutos[[n]]=xAutos[[n]]+vAutos[[n]]-nCells],{n,1,nCar}];
-(**\[CapitalUDoubleDot]berpr\[UDoubleDot]f, ob die Tr\[ODoubleDot]delnwahrscheinligkeit (p) null oder eins ist also ob das auto tr\[ODoubleDot]delt oder nicht **)
+
 (*Falls Autos au\[SZ]erhalb Zellen bewegt, wird Bewegung in erster Zelle fortgesetzt, da Ringstra\[SZ]e*)
 
 (*
@@ -123,8 +121,14 @@ ListPlot[density]
 ]
 
 
+
 densityplot[7,30,15,5,0.3,10]
 (*Table[density[[t]],{t,1,15}]*)
+
+
+
+(* ::Text:: *)
+(*-Histogramme von Geschwindigkeit und *)
 
 
 
