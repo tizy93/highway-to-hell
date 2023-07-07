bb4f285f301c51f12209cf6617f941a2f85b6cd0
tizy93 <tiziana.ricciardi93@gmail.com>
2023-07-07 15:14:26 +0200

    fluss mit fehler

--- NaSch-Tiz.m
+++ NaSch-Tiz.m
@@ -4,13 +4,63 @@
 (**)
 
 
+(*Dichteplot \[UDoubleDot]ber Zeit*)
+densityplot[nCar_,nCells_,tMax_,vMax_,p_,avCells_]:=Module[
+(*lokale Variablen*)
+{xAutos,vAutos,dAutos,regionCars,density},
+
+(*NaSch-Modell*)
+
+(*Autos haben Position x und Geschwindigkeit v zum vorderen Auto*)
+xAutos=Sort[RandomSample[Range[nCells],nCar]];
+vAutos=RandomInteger[{0,vMax},nCar]; 
+
+(*Erzeugen einelementige Liste mit Dichte*) 
+density=Table[Nothing,{n,1}];
+
+(*Verkehrsregeln aus NaSch-Modell implementieren*)
+For[i=0,i<=tMax,i++, 
+
+(*Freie Zellen d vor dem Auto bis zum vorderen*)
+dAutos=Table[If[xAutos[[n]]<Max[xAutos],xAutos[[n+1]]-xAutos[[n]]-1,nCells-xAutos[[n]]+Min[xAutos]-1],{n,1,nCar-1}]; 
+AppendTo[dAutos,If[xAutos[[nCar]]<Max[xAutos],xAutos[[1]]-xAutos[[nCar]]-1,nCells-xAutos[[nCar]]+Min[xAutos]-1]];
+
+(*R1: Beschleunigen, falls vMax noch nicht erreicht*)
+vAutos=Table[Min[vAutos[[n]]+1,vMax],{n,1,nCar}];
+
+(*R2: Abbremsen, falls v gr\[ODoubleDot]\[SZ]er als Abstand d*)
+vAutos=Table[Min[dAutos[[n]],vAutos[[n]]],{n,1,nCar}]; 
+
+(*R3: Tr\[ODoubleDot]deln mit Wahrscheinlichkeit p*)
+vAutos=Table[If[RandomReal[{0,1}]<=p,vAutos[[n]]=Max[vAutos[[n]]-1,0],vAutos[[n]]],{n,1,nCar}]; 
+
+(*R4: Fahren um vAutos Zellen*)
+xAutos=Table[If[xAutos[[n]]+vAutos[[n]]<=nCells,xAutos[[n]]=xAutos[[n]]+vAutos[[n]],xAutos[[n]]=xAutos[[n]]+vAutos[[n]]-nCells],{n,1,nCar}];
+
+(*Erstellen Liste mit Autos innerhalb vorgegebener Region von der ersten Zelle bis zur frei w\[ADoubleDot]hlbaren Zelle avCells*)
+regionCars=DeleteCases[Table[If[xAutos[[n]]<=avCells,xAutos[[n]],],{n,1,nCar}],Null]; (*Nullen mit DeleteCases gel\[ODoubleDot]scht*)
+(*Element zu density-Liste mit Dichte am Zeitpunkt t hinzugef\[UDoubleDot]gt, t entspricht Reihenfolge der Liste*)
+AppendTo[density,Length[regionCars]/avCells];
+(*Ausgabe xAutos*)
+(*
+Print[xAutos]; (*Print kostet viel Rechenzeit*)
+*)
+]
+(*Plotten Dichte*)
+Delete[density,1]; (*L\[ODoubleDot]schen erstes Nullelement*)
+(*Print[density];*)
+ListPlot[density]
+]
+
+
+
 (*Modul Nagel-Schreckenberg Modell*)
 Meanvarfluss[nCar_, nCells_, tMax_, vMax_, p_] :=
     Module[
       (*Eingabe Anzahl der Autos nCar, Anzahl der Zellen nCells, Simulationsdauer
      tMax, 
 Maximalgeschwindigkeit vMax und Tr\[ODoubleDot]delwahrscheinlichkeit p mit Funktionsaufruf
-    *)(*lokale Variablen*){xAutos, vAutos, dAutos, vMittel, dVar,t,dMittel,m}
+    *)(*lokale Variablen*){xAutos, vAutos, dAutos, vMittel, dVar,t,dMittel,m,fluss}
         ,
         (*Autos haben Position x und Geschwindigkeit v zum vorderen Auto
             *)
@@ -20,10 +70,13 @@ Maximalgeschwindigkeit vMax und Tr\[ODoubleDot]delwahrscheinlichkeit p mit Funkt
             )**)
         vAutos = RandomInteger[{0, vMax}, nCar];
         (**zuordnet eine zuf\[ADoubleDot]llige Zahl zu jedes Auto **)
+        (*Mittelgeschwindigkeit*)
          vMittel = Table [Nothing, {t,1}];
         (*dMittel= Table[Nothing , {m,1}];*)
+        (*varianz dAuto*)
         dVar = Table[Nothing, {t,1}];
-        
+        (*Verkehrsfluss*)
+        fluss=Table[Nothing,{n,1}];
         
         (*Verkehrsregeln aus NaSch-Modell implementieren*)
         For[i = 0, i <= tMax, i++,(*Schleife der Runden bis tMax*)(*Freie
@@ -59,18 +112,20 @@ Maximalgeschwindigkeit vMax und Tr\[ODoubleDot]delwahrscheinlichkeit p mit Funkt
         (*mittlere geschwindigkeit*)
         AppendTo[vMittel, N[Mean[vAutos],6]];
         (*AppendTo[dMittel, N[Mean[dAutos],6]];*)
+        (*varianz dAuto*)
         AppendTo[dVar, N[Variance[dAutos],6]];
-       Print[dAutos];
+        AppendTo[density]
+        
         ]
         Print[vMittel];
         (*dVar= N[Variance[dMittel],7];*)
         Print[dVar];
         (*Print[dMittel];*)
+        
         ListPlot[vMittel]
         ListPlot[dVar]
      ]
-     (*bla bla bla*)
-    (*test marie*) 
+     
    (*value={1,2,2,3,4,5,6}
    Mittel=Mean [value]
    var=Variance[value]*)
@@ -79,5 +134,8 @@ Maximalgeschwindigkeit vMax und Tr\[ODoubleDot]delwahrscheinlichkeit p mit Funkt
 
 Meanvarfluss[30,100,20,10,0.5]
 
-(*try marie*)
+
+
+
+
 
