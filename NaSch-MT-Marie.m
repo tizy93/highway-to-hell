515e19d2d3fa7d24487e01f3c23b631a0ca3e7de
mcode0 <m.acc@tetenbuell-city.de>
2023-07-07 17:48:01 +0200

    Fundamentalplot mit Frage nach Fluss

--- NaSch-MT-Marie.m
+++ NaSch-MT-Marie.m
@@ -218,15 +218,15 @@ vAutos=Table[If[vAutos[[n]]==0,If[RandomReal[{0,1}]<=p+0.1,vAutos[[n]],vAutos[[n
 (*R4: Fahren um vAutos Zellen*)
 xAutos=Table[If[xAutos[[n]]+vAutos[[n]]<=nCells,xAutos[[n]]=xAutos[[n]]+vAutos[[n]],xAutos[[n]]=xAutos[[n]]+vAutos[[n]]-nCells],{n,1,nCar}];
 
-(*VDR: beim Anfahren nur mit einer Wahrscheinlichkeit p+q anfahren*)
+(*VDR: beim Anfahren nur mit einer Wahrscheinlichkeit p+q anfahren, hier q=0.1 gew\[ADoubleDot]hlt -> lieber Eingabe?*)
 ]
 ]
 
 
 (*Berechnung mittlere v \[UDoubleDot]ber t, Varianz des mittleren Abstands und des Verkehrsflusses \[UDoubleDot]ber t*)
-Mittelv[nCar_,nCells_,tMax_,vMax_,p_]:=Module[
+Meanvarfluss[nCar_,nCells_,tMax_,vMax_,p_,posCell_]:=Module[
 (*lokale Variablen*)
-{xAutos, vAutos, dAutos, vMittel, dVar,t,dMittel,m},
+{xAutos, vAutos, dAutos, vMittel, dVar,t,dMittel,m,fluss,regionCars},
 
 (*NaSch-Modell*)
 
@@ -234,9 +234,11 @@ Mittelv[nCar_,nCells_,tMax_,vMax_,p_]:=Module[
 xAutos=Sort[RandomSample[Range[nCells],nCar]];
 vAutos=RandomInteger[{0,vMax},nCar]; 
 
-(*Erzeugen einelementige Liste mit vMittel und dMittel*) 
+(*Erzeugen einelementige Liste mit vMittel, dVar und fluss*) 
+Clear[vMittel];
 vMittel = Table [Nothing, {t,1}];
-dMittel= Table[Nothing , {m,1}];
+dVar = Table[Nothing, {t,1}];
+fluss=Table[Nothing,{n,1}];
 
 (*Verkehrsregeln aus NaSch-Modell implementieren*)
 For[i=0,i<=tMax,i++, 
@@ -257,18 +259,90 @@ vAutos=Table[If[RandomReal[{0,1}]<=p,vAutos[[n]]=Max[vAutos[[n]]-1,0],vAutos[[n]
 (*R4: Fahren um vAutos Zellen*)
 xAutos=Table[If[xAutos[[n]]+vAutos[[n]]<=nCells,xAutos[[n]]=xAutos[[n]]+vAutos[[n]],xAutos[[n]]=xAutos[[n]]+vAutos[[n]]-nCells],{n,1,nCar}];
 
-AppendTo[vMittel, N[Mean[vAutos],4]];
-AppendTo[dMittel, N[Mean[dAutos],4]];
-Print[dAutos];
-];
+(*mittlere geschwindigkeit*)
+AppendTo[vMittel, N[Mean[vAutos],6]];
+(*Varianz dAutos*)
+AppendTo[dVar, N[Variance[dAutos],6]];
+(*Verkehrsfluss*)
+regionCars=DeleteCases[Table[If[xAutos[[n]]==posCell,xAutos[[n]],],{n,1,nCar}],Null];
+AppendTo[fluss,Length[regionCars]];
+]
 Print[vMittel];
-dVar= N[Variance[dMittel],10];
 Print[dVar];
-Print[dMittel];
+Print[fluss];
+ListPlot[vMittel,ImageSize->Medium,ColorFunction->"Rainbow",AxesLabel->{"Zeit t",mittlere Geschwindigkeit OverBar[v]}]
+ListPlot[dVar,ImageSize->Medium,ColorFunction->"Rainbow",AxesLabel->{"Zeit t",Varianz des Abstands d}]
+ListPlot[fluss,ImageSize->Medium,ColorFunction->"Rainbow",AxesLabel->{"Zeit t","Fluss \[UDoubleDot]ber Zelle}]
 ]
 
 
-Mittelv[30,100,20,5,0.5]
+Meanvarfluss[30,100,20,5,0.5,4]
+
+
+
+
+(*Fundamentalplot*)
+FundamentalD[nCar_,nCells_,tMax_,vMax_,p_,avCells_]:=Module[
+(*lokale Variablen*)
+{xAutos, vAutos, dAutos, dMittel, fluss, regionCars, cellCars, density, fundamD,mittelFluss,tMittelF,tcellCars,tfluss},
+
+(*NaSch-Modell*)
+
+(*Autos haben Position x und Geschwindigkeit v zum vorderen Auto*)
+xAutos=Sort[RandomSample[Range[nCells],nCar]];
+vAutos=RandomInteger[{0,vMax},nCar]; 
+
+(*Erzeugen einelementige Liste mit Dichte, Fluss, dem Mittelwert des Flusses und des zeitlichen Mittel des Flusses*) 
+density=Table[Nothing,{n,1}];
+fluss=Table[Nothing,{n,1}];
+mittelFluss=Table[Nothing,{n,1}];
+tfluss=Table[Nothing,{n,1}];
+
+(*Verkehrsregeln aus NaSch-Modell implementieren*)
+For[i=0,i<=tMax,i++, 
+
+(*Freie Zellen d vor dem Auto bis zum vorderen*)
+dAutos=Table[If[xAutos[[n]]<Max[xAutos],xAutos[[n+1]]-xAutos[[n]]-1,nCells-xAutos[[n]]+Min[xAutos]-1],{n,1,nCar-1}]; 
+AppendTo[dAutos,If[xAutos[[nCar]]<Max[xAutos],xAutos[[1]]-xAutos[[nCar]]-1,nCells-xAutos[[nCar]]+Min[xAutos]-1]];
+
+(*R1: Beschleunigen, falls vMax noch nicht erreicht*)
+vAutos=Table[Min[vAutos[[n]]+1,vMax],{n,1,nCar}];
+
+(*R2: Abbremsen, falls v gr\[ODoubleDot]\[SZ]er als Abstand d*)
+vAutos=Table[Min[dAutos[[n]],vAutos[[n]]],{n,1,nCar}]; 
+
+(*R3: Tr\[ODoubleDot]deln mit Wahrscheinlichkeit p*)
+vAutos=Table[If[RandomReal[{0,1}]<=p,vAutos[[n]]=Max[vAutos[[n]]-1,0],vAutos[[n]]],{n,1,nCar}]; 
+
+(*R4: Fahren um vAutos Zellen*)
+xAutos=Table[If[xAutos[[n]]+vAutos[[n]]<=nCells,xAutos[[n]]=xAutos[[n]]+vAutos[[n]],xAutos[[n]]=xAutos[[n]]+vAutos[[n]]-nCells],{n,1,nCar}];
+
+(*Versuch zeitliches Mittel des Flusses*)
+tcellCars=DeleteCases[Table[If[xAutos[[n]]==avCells,xAutos[[n]],],{n,1,nCar}],Null];
+AppendTo[tfluss,Length[cellCars]];
+
+(*Verkehrsfluss gemittelt \[UDoubleDot]ber Zellen 1 bis avCells*)
+cellCars=Select[DeleteCases[Table[DeleteCases[Table[If[xAutos[[n]]==m,xAutos[[n]],],{n,1,nCar}],Null],{m,1,avCells}],Null],UnsameQ[#,{}]&];
+AppendTo[fluss,Length[cellCars]];
+AppendTo[mittelFluss, Mean[fluss]];
+
+(*Dichte von Zellen 1 bis avCells*)
+regionCars=DeleteCases[Table[If[xAutos[[n]]<=avCells,xAutos[[n]],],{n,1,nCar}],Null]; (*Nullen mit DeleteCases gel\[ODoubleDot]scht*)
+AppendTo[density,Length[regionCars]/avCells];
+
+](*tMittelF=Mean[tfluss];
+Print[tMittelF];*)
+Print[fluss];
+Print[mittelFluss];
+Print[density];
+(*Print[Thread[{density,mittelFluss}]];*)
+ListPlot[density,ImageSize->Medium,AxesLabel->Dichte]
+ListPlot[mittelFluss,ImageSize->Medium,AxesLabel->Mittelfluss]
+ListPlot[Thread[{density,mittelFluss}],ImageSize->Medium,AxesLabel->{Dichte,Mittelfluss}]
+]
+
+
+FundamentalD[30,100,20,10,0.3,10]
 
 
 
